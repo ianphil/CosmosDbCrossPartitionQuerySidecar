a143c67cc1da618ff96b09438f72a9e4e78a360c
Ian Philpot <ianphil@microsoft.com>
2022-05-05 00:54:12 -0400

    Added CosmosService that fails cross partition queries

--- src/Service/services/DatabaseServices.ts
+++ src/Service/services/DatabaseServices.ts
@@ -1,15 +1,41 @@
+import { Container, CosmosClient, Database } from "@azure/cosmos";
 import { Address } from "../models/Address";
 import { IDatabaseService } from "./IDatabaseService";
 
 export class CosmosService implements IDatabaseService {
-    upsert(address: Address): Promise<Address> {
-        throw new Error("Method not implemented.");
+    private readonly _cosmosClient: CosmosClient;
+    private _db: Database | undefined;
+    private _container: Container | undefined;
+
+    constructor() {
+        this._cosmosClient = new CosmosClient(process.env.CONN_STRING!);
     }
-    queryByZip(zipCode: string): Promise<Address[]> {
-        throw new Error("Method not implemented.");
+
+    async upsert(address: Address): Promise<Address> {
+        await this.init();
+        return (await this._container!.items.upsert<Address>(address)).resource as Address;
     }
-    queryByState(state: string): Promise<Address[]> {
-        throw new Error("Method not implemented.");
+
+    async queryByZip(zipCode: string): Promise<Address[]> {
+        await this.init();
+        let q: string = `SELECT * FROM c`;
+        return (await this._container!.items.query(q, { partitionKey: zipCode }).fetchAll()).resources as Address[];
+    }
+
+    async queryByState(state: string): Promise<Address[]> {
+        await this.init();
+        let q: string = `SELECT * FROM c WHERE c.state = ${state}`;
+        return (await this._container!.items.query<Address>(q).fetchAll()).resources as Address[];
+    }
+
+    private async init() {
+        if (this._db == undefined) {
+            this._db = (await this._cosmosClient.databases.createIfNotExists({ id: process.env.DB_NAME! })).database;
+        }
+
+        if (this._container == undefined) {
+            this._container = (await this._db!.containers.createIfNotExists({ id: process.env.COLL_NAME! })).container;
+        }
     }
 
 }
